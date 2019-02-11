abbe585cc29e203798e650a78025d66c1540522a
Donnie Toombs <donnietoombs@gmail.com>
2019-02-10 19:28:22 -0500

    update upload-portfolio-lambda python file

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,21 +5,30 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:777156750427:deployPortfolioTopic')
     
-    portfolio_bucket=s3.Bucket('portfolio.donaldtoombs.com')
-    build_bucket = s3.Bucket('portfolio-build-donaldtoombs.com')
-    
-    portfolio_zip = StringIO.StringIO(0)
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm, 
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-    print "Job Done"
-
+    try:
+        s3 = boto3.resource('s3')
+        
+        portfolio_bucket=s3.Bucket('portfolio.donaldtoombs.com')
+        build_bucket = s3.Bucket('portfolio-build-donaldtoombs.com')
+        
+        portfolio_zip = StringIO.StringIO(0)
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm, 
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+            
+        print "Job Done"
+        topic.publish(Subject= "Portfolio Deployed", Message ="Portfolio Deployed Successfully")
+    except:
+        topic.publish(Subject= "Portfolio Deploy Failed", Message ="Portfolio was not deployed successfully!")
+        raise
+        
     return 'Hello from Lambda'
+
